cf7c3038863032b5b6c02f28fa3cfbefc70cb249
Piyush Makwana <piyushmakwana@outlook.in>
2023-12-10 11:12:57 +0530

    added by piyush

--- app/src/main/java/com/example/dopamine/Dopamine_home.kt
+++ app/src/main/java/com/example/dopamine/Dopamine_home.kt
@@ -1,12 +1,11 @@
 package com.example.dopamine
 
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.example.dopamine.databinding.ActivityDopamineHomeBinding
 import com.google.firebase.auth.FirebaseAuth
+
 
 class Dopamine_home : AppCompatActivity() {
     private lateinit var binding: ActivityDopamineHomeBinding
@@ -23,7 +22,12 @@ class Dopamine_home : AppCompatActivity() {
         //binding.UserName.text = googleSession.sharedPreferences.getString("Mon","")
 
         //Photo Fetching
-        val userPhoto = googleSession.sharedPreferences.getString("photo","")
-        Glide.with(this).load(userPhoto).into(binding.UserImage)
+        if(googleSession.sharedPreferences.getString("Mon","")!=null){
+            val userPhoto = "https://uxwing.com/wp-content/themes/uxwing/download/peoples-avatars/default-profile-picture-grey-male-icon.png"
+            Glide.with(this).load(userPhoto).into(binding.UserImage)
+        }else{
+            val userPhoto = googleSession.sharedPreferences.getString("photo","")
+            Glide.with(this).load(userPhoto).into(binding.UserImage)
+        }
     }
 }
