c7c744f662ca9d3a11ad8d4d3cff6d87d79f8032
Piyush Makwana <piyushmakwana@outlook.in>
2023-12-15 07:58:25 +0530

    update by piyush

--- app/src/main/java/com/example/dopamine/DopamineMuiscPlayer/MasterMusicPlayer.kt
+++ app/src/main/java/com/example/dopamine/DopamineMuiscPlayer/MasterMusicPlayer.kt
@@ -3,10 +3,20 @@ package com.example.dopamine.DopamineMuiscPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.dopamine.R
+import com.example.dopamine.databinding.ActivityDopamineTracksBinding
+import com.example.dopamine.databinding.ActivityMasterMusicPlayerBinding
 
 class MasterMusicPlayer : AppCompatActivity() {
+    private lateinit var binding: ActivityMasterMusicPlayerBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_master_music_player)
+        binding = ActivityMasterMusicPlayerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.blankTitle.setOnFocusChangeListener { v, hasFocus ->
+            if(!hasFocus){
+                binding.blankTitle.isSelected = true
+            }
+        }
     }
 }
