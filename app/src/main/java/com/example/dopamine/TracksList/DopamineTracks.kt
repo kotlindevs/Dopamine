1315574aa637c928191fd5e1cfb5bcc68d5182f9
like google <kotlindevslife@gmail.com>
2023-12-16 10:55:37 +0530

    ui done

--- app/src/main/java/com/example/dopamine/TracksList/DopamineTracks.kt
+++ app/src/main/java/com/example/dopamine/TracksList/DopamineTracks.kt
@@ -2,6 +2,7 @@ package com.example.dopamine.TracksList
 
 import android.os.Bundle
 import android.util.Log
+import android.view.animation.ScaleAnimation
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.dopamine.TracksList.Adapter.TrackListAdapter
@@ -43,5 +44,6 @@ class DopamineTracks : AppCompatActivity() {
                 }
 
             })
+
     }
 }
