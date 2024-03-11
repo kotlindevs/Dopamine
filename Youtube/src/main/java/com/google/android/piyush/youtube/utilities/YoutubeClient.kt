45f61d83f42fe0b24273930bcb8d403c67538db2
kevat <kotlindevslife@gmail.com>
2024-03-11 20:45:53 +0530

    :)

--- Youtube/src/main/java/com/google/android/piyush/youtube/utilities/YoutubeClient.kt
+++ Youtube/src/main/java/com/google/android/piyush/youtube/utilities/YoutubeClient.kt
@@ -50,26 +50,30 @@ object YoutubeClient {
     const val SEARCH_PART = "snippet"
 
     val REVERB_AND_SLOWED = arrayListOf( // coding videos
-        "PLfqMhTWNBTe0PY9xunOzsP5kmYIz2Hu7i", "PLBTanuC8SLeZaIXwjc-vDZ-CmRak-KITf",
+        "PLfqMhTWNBTe0PY9xunOzsP5kmYIz2Hu7i","PLfqMhTWNBTe0gqgFk-CUE-ktO5Cek1GdP",
+        "PLfqMhTWNBTe0sPLFF91REaJQEteFZtLzA","PLfqMhTWNBTe25HU2y-3Kx6MBsasawd61U",
+        "PLfqMhTWNBTe3LtFWcvwpqTkUSlB32kJop","PLfqMhTWNBTe3H6c9OGXb5_6wcc1Mca52n",
+        "PLu0W_9lII9ahR1blWXxgSlL4y9iQBnLpR","PLu0W_9lII9aikXkRE0WxDt1vozo3hnmtR",
+        "PLu0W_9lII9aiL0kysYlfSOUgY5rNlOhUd","PLRAV69dS1uWQGDQoBYMZWKjzuhCaOnBpa",
+        "PLjVLYmrlmjGcQfNj_SLlLV4Ytf39f8BF7","PL5PR3UyfTWvfacnfUsvNcxIiKIgidNRoW",
+        "PLai5B987bZ9CoVR-QEIN9foz4QCJ0H2Y8","PL9ooVrP1hQOG6DQnOD6ujdCEchaqADfCU",
     ).random()
 
     val GAMING_VIDEOS = arrayListOf( // sports videos
-        "PL3tRBEVW0hiBSFOFhTC5wt75P2BES0rAo", "PLe8jmEHFkvsasy1XhS5w4rWpvwBvOSFL0",
-        "PLBUBxRIrD2exTPnwjcS_H130mD1srsHW0", "PLqVvtGEPyn1xplqlxx_9BSCQ46lF42tLy",
-        "PLKTyKnsCvPUYwEkqueADvfX0aHx88ndCt", "PLhdhcwETqX-BqbssnCUS32dQ3fXE9fjk9",
-        "PLhJUcbNkR99aSzTAMwZVoWhqA6kIfxd6Q", "PLRnX6J5CPQSwOHqOASvPKr_Uvbzddj7h4",
-        "PL-TaHOq8pJ-a9S-EWx9MmQs-8Em1-vQ-k", "PLFfYcxV5z_c5zM2zPvGbFHfehUSIuvpx0",
-        "PLRaYMMxEZyMXcSrQRh2xZulbfNpxYIAiJ", "PLzXmNUicjAvy_81u9RzZdMUpCFuXO1Ihl",
+        "PLCGIzmTE4d0hhfpwtaWR-mgtRZmsy5e4t","PLn5vww_8o5KuZBHmg673Qmy7FdBKiKxqg",
+        "PLfoNZDHitwjX-oU5YVAkfuXkALZqempRS","PLu8uIfcaMMW58ql_wEWJ8VJ0Bs8TN5DWP",
+        "PLrfBm83TZJt-t6SOdimRizi3_hyHyj83G","PL1NbHSfosBuG_HB8WTgFaCVsDWCcnOsUW",
+        "PLQHHr8gPOsH48qDASzYskAYiWHgKhPl26","PLHKIZtgW3Stx7FBBV1fU5EziT_2HCGciV",
     ).random()
 
     val LOFI_BHAJAN =  arrayListOf( // tech videos
-        "PLuH-I1ovyzeioJ_j4DcQgoxtAbgBc3YD2", "PL6pXmupaeNZ2q9xnEroqVyZwvwXygFWPJ",
-        "PL5RtZw3_Rjtx-4F5obOXpSuAVWsfVzmbf", "PLhmcRXNItagwuwlaO3slDOOLsLZCmrusg",
-        "PLvc9tfarNFkXRyEcWzsHecc8T4TcZY4kt", "PLPhN6_svX3lJL5t8qziY1EwfgyfbKOsYq",
-        "PLEtkiKkqwTOE526XvoQDwkcnqIV506edf", "PLmn9r2NQijPAAgQvVFOHvl4tejc7Gb5EZ",
-        "PLgcK2qYcWCwRpG8CuuDSmIcKtzc426ukn", "PLPpmV2P0FYbLxP04VwXpZT4jDbUrUaX8v",
-        "PLcMQbgxnh9hh9XK-y_0LCyU2N3FpAfDz2", "PLhwUrf4RX562HgDUMWiSrclejuBoCk7yD",
-        "PLdbOyAhlSn6THjqQ263k8qnWOLkaAePP8", "PLiae937rUykQrQWLjNzDK4AA1OjS92v17",
+        "PLflqtq8EOGAJJDNAct-tz9X8C6-MSljjB","PLBsP89CPrMeOYPjeabTfPW8UDC-WRn2Gi",
+        "PLG2K6CpAgCSqTTu87JRtBXnxgbXU_BqVy","PLWMr6-kiy-EymYr84AW65YK5HQqa3c3pM",
+        "PLubgCncKqwofL7DwwOZ4PisRcVQcbn8A1","PL0W2eFwhS9h6Cw0PUmKFoTiKAITx0PMWu",
+        "PL54E1sm59KWNyAKQ_UDUKf73pAnhyhg93","PLHcmK2sS7-mScUCt2qPtYFDrEd2uC_h7q",
+        "PLNyLhAZuODp3k1-V8eZp2J3aQU_82TAaT","PLMYDmB_aqcVaduG-18hAfh6NEN6iAZuDX",
+        "PLSBvbeScevHqnaRfybw-Jtuf14T_KCsNZ","PL1htDegCU2nViVRvpYlI67llR49eUm5I9",
+        "PLM6PG6pQfVYHlhE0GbtmciPBu4NIiQoJ4","PLBsP89CPrMeM2MmF4suOeT0vsic9nEC2Y"
     ).random()
 
     val INDIAN_BOLLEYWOOD_PLAYLIST = arrayListOf(
