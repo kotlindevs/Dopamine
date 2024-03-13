78dc6bc09f0f41d7d985ccbd9ff450241ff81ff2
Like Google <kotlindevslife@gmail.com>
2024-03-14 01:21:47 +0530

    search bar added

--- app/src/main/java/com/google/android/piyush/dopamine/adapters/SearchAdapter.kt
+++ app/src/main/java/com/google/android/piyush/dopamine/adapters/SearchAdapter.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.piyush.dopamine.R
 import com.google.android.piyush.dopamine.activities.YoutubeChannel
 import com.google.android.piyush.dopamine.activities.YoutubePlayer
@@ -37,28 +38,51 @@ class SearchAdapter(
             .into(holder.image)
         holder.text1.text = video?.snippet?.title
         holder.text2.text = video?.snippet?.channelTitle
-        holder.channelview.setOnClickListener {
-            Toast.makeText(context, video?.kind, Toast.LENGTH_SHORT).show()
-            context.startActivity(
-                Intent(
-                    context,
-                    YoutubeChannel::class.java
-                )
-                    .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                    .putExtra("channelId", video?.snippet?.channelId)
-            )
-        }
-        holder.video.setOnClickListener {
-            Toast.makeText(context, video?.kind, Toast.LENGTH_SHORT).show()
-            context.startActivity(
-                Intent(
-                    context,
-                    YoutubePlayer::class.java
-                )
-                    .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                    .putExtra("videoId", video?.id?.videoId)
-                    .putExtra("channelId", video?.snippet?.channelId)
-            )
+            holder.channelview.setOnClickListener {
+                if(video?.snippet?.channelId.isNullOrEmpty()){
+                    MaterialAlertDialogBuilder(context).apply {
+                        this.setTitle("Error")
+                        this.setMessage("Channel Id is null or channel not found")
+                        this.setIcon(R.drawable.ic_dialog_error)
+                        this.setCancelable(true)
+                        this.setNegativeButton("Okay") { dialog, _ ->
+                            dialog?.dismiss()
+                        }
+                    }.create().show()
+                }else {
+                    context.startActivity(
+                        Intent(
+                            context,
+                            YoutubeChannel::class.java
+                        )
+                            .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                            .putExtra("channelId", video?.snippet?.channelId)
+                    )
+                }
+            }
+
+            holder.video.setOnClickListener {
+                if(video?.id?.videoId.isNullOrEmpty() || video?.snippet?.channelId.isNullOrEmpty()){
+                    MaterialAlertDialogBuilder(context).apply {
+                        this.setTitle("Error")
+                        this.setMessage("Either video id or channel id is null")
+                        this.setIcon(R.drawable.ic_dialog_error)
+                        this.setCancelable(true)
+                        this.setNegativeButton("Okay") { dialog, _ ->
+                            dialog?.dismiss()
+                        }
+                    }.create().show()
+                }else {
+                    context.startActivity(
+                        Intent(
+                            context,
+                            YoutubePlayer::class.java
+                        )
+                            .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                            .putExtra("videoId", video?.id?.videoId)
+                            .putExtra("channelId", video?.snippet?.channelId)
+                    )
+                }
         }
     }
 }
