c6e1d9fad21f2def7abefbf8cc0dc5d4c6c535d0
kevat <kotlindevslife@gmail.com>
2024-03-14 21:41:25 +0530

    :)

--- app/src/main/java/com/google/android/piyush/dopamine/activities/YoutubePlayer.kt
+++ app/src/main/java/com/google/android/piyush/dopamine/activities/YoutubePlayer.kt
@@ -143,7 +143,7 @@ class YoutubePlayer : AppCompatActivity() {
             override fun onEnterFullscreen(fullscreenView: View, exitFullscreen: () -> Unit) {
                 requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
                 window.decorView.systemUiVisibility =
-                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN
+                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
 
                 listOf(
                     binding.YtPlayer,
@@ -410,6 +410,20 @@ class YoutubePlayer : AppCompatActivity() {
         if (requestCode == 1) {
             if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 val notification = notificationBuilder.build()
+                if (ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.POST_NOTIFICATIONS
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return
+                }
                 notificationManager.notify(1, notification)
             } else {
                 ToastUtilities.showToast(
