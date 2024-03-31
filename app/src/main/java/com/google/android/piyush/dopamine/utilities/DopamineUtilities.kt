980c01344cfb525933a2a256faec9aac8056084d
Like Google <kotlindevslife@gmail.com>
2024-03-31 19:39:16 +0530

    dopamine_20243103_02.phone.stable.dynamic

--- app/src/main/java/com/google/android/piyush/dopamine/utilities/DopamineUtilities.kt
+++ app/src/main/java/com/google/android/piyush/dopamine/utilities/DopamineUtilities.kt
@@ -41,8 +41,8 @@ object Utilities {
 const val PERMISSION_REQUEST_CODE = 100
     const val PROCESS_ID = "MyDlProcess"
     const val PROJECT_ID = "com.google.android.piyush.dopamine"
-    const val RELEASE_DATE = "26/03/2024"
-    const val PROJECT_VERSION = "dopamine_20242703_03.phone.stable.dynamic"
+    const val RELEASE_DATE = "31/03/2024"
+    const val PROJECT_VERSION = "dopamine_20243103_02.phone.stable.dynamic"
     const val PRE_RELEASE_VERSION = "dopamine_20242003_01.phone.prerelease.dynamic"
     const val STABLE = "stable"
     const val DEFAULT_LOGO = "https://cdn-images-1.medium.com/v2/resize:fit:1200/1*3tLD4Ve66pbBpuawm9Fu9Q.png"
