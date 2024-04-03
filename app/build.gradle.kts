0c6d2ad5d2d6d6ff2bad184aeeb1c198e0108823
Like Google <kotlindevslife@gmail.com>
2024-04-04 00:48:55 +0530

    Initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 1
-        versionName = "version dopamine_20240204_07.phone.prerelease.dynamic"
+        versionName = "version dopamine_20240404_04.phone.prerelease.dynamic"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
