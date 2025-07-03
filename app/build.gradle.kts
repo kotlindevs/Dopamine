da56be642d7504ec2750c3fc59634223b6b5a6f5
Kotlin Crafter <likegoogle@outlook.co.id>
2025-07-03 11:00:57 +0530

    Rebuild dopamine

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,6 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.android)
     id("com.google.dagger.hilt.android")
     id("com.google.devtools.ksp")
-//    id("org.jetbrains.kotlin.plugin.compose")
 }
 
 android {
@@ -41,12 +40,8 @@ android {
         languageVersion = "2.2"
     }
     buildFeatures {
-//        compose = true
         viewBinding = true
     }
-//    composeOptions {
-//        kotlinCompilerExtensionVersion = "1.5.15"
-//    }
 }
 
 dependencies {
@@ -61,14 +56,6 @@ dependencies {
     implementation(libs.core)
     implementation(project(":Youtube"))
     implementation(project(":Database"))
-
-    // compose
-//    implementation("androidx.activity:activity-compose:1.10.1")
-//    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.1")
-//    implementation("androidx.compose.ui:ui-tooling-preview:1.8.3")
-//    implementation("androidx.compose.ui:ui:1.8.3")
-//    implementation("androidx.compose.ui:ui-viewbinding:1.8.3")
-//    implementation("androidx.compose.material3:material3:1.3.2")
 
     implementation(libs.hilt.android)
     ksp(libs.hilt.compiler)
--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.android.library) apply false
     kotlin("jvm") version "2.2.0"
-//    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0"
     id("com.google.devtools.ksp") version "2.2.0-2.0.2" apply false
     id("com.google.dagger.hilt.android") version "2.56.2" apply false
 }
