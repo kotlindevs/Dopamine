2aa131323b51efb55a1451a5a01d4420567ec8f1
Kotlin Crafter <likegoogle@outlook.co.id>
2025-07-04 17:51:40 +0530

    Rebuild dopamine

--- app/src/main/java/com/google/android/piyush/dopamine/DopamineApp.kt
+++ app/src/main/java/com/google/android/piyush/dopamine/DopamineApp.kt
@@ -1,7 +1,16 @@
 package com.google.android.piyush.dopamine
 
 import android.app.Application
+import android.os.Build
+import com.google.android.material.color.DynamicColors
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class DopamineApp : Application()
+class DopamineApp : Application(){
+    override fun onCreate() {
+        super.onCreate()
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.S){
+            DynamicColors.applyToActivitiesIfAvailable(this)
+        }
+    }
+}
